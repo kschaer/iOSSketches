932dac91873bad276240766b9a6fda30930d3495
kschaer <kaitlin.schaer@gmail.com>
2017-11-04 20:36:09 -0400

    separate classes

--- spirograph1/src/ofApp.h
+++ spirograph1/src/ofApp.h
@@ -14,6 +14,8 @@ public:
     
     void touchDown(ofTouchEventArgs & touch);
     void touchMoved(ofTouchEventArgs & touch);
+    
+    
     void touchUp(ofTouchEventArgs & touch);
     void touchDoubleTap(ofTouchEventArgs & touch);
     void touchCancelled(ofTouchEventArgs & touch);
@@ -23,46 +25,8 @@ public:
     void gotMemoryWarning();
     void deviceOrientationChanged(int newOrientation);
     
-    bool bSmooth;
-    float counter;
-    
-    ofTrueTypeFont myFont;
-    
-    float time;
-    float sinTime;
-    
-    float width;
-    float height;
-    
-    string myText;
-    float posY;
-    float posX;
-    ofPoint centerText;
-    ofRectangle textBounding;
-    
-    vector <vector <ofVec2f> > points;
-    vector <vector <ofVec2f> > points2;
-    
-    ofPath outline;
-    
-    float angle;
-    int numPoints;
-    float radius;
-    ofPoint circCenter;
-    ofPoint circPoints[75];
-    
-    float chaos1;
-    float chaos2;
-    
-    int spaceCount;
-    
-    float lightness;
-    
-    float nextPointSpeed;
-    
-    int frameCount;
-    
-    float rotateSpeed;
+
+    spirograph1 spiroText;
     
 };
 
--- spirograph1/src/scene.m
+++ spirograph1/src/scene.m
@@ -1,8 +1,8 @@
 //
-//  spirograph1.m
+//  scene.m
 //  spirograph1
 //
-//  Created by Kaitlin Schaer on 11/3/17.
+//  Created by Kaitlin Schaer on 11/4/17.
 //
 
 #import <Foundation/Foundation.h>
